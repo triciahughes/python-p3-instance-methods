ba5cc51d36d7de822290c134c8ad024dda7aa83f
triciahughes <hughes.a.tricia@gmail.com>
2023-02-22 17:07:57 -0800

    completed lab

--- lib/dog.py
+++ lib/dog.py
@@ -4,4 +4,16 @@ class Dog:
     # Class body goes here
 
     #Instance method definition
+    def bark(self):
+        print("Woof!")
     pass
+
+    def sit(self):
+        print("The dog is sitting.")
+
+fido = Dog()
+fido.bark()
+fido.sit()
+
+snoopy = Dog()
+snoopy.bark()
